24d5333987bfd9347242fdb6897020c1cf89aa3e
Ben Wolf <benjamin.wolf@innoq.com>
2018-04-19 13:50:04 +0200

    Fix expression and Pf3ilz0r

--- SharpestChain/SharpestChain/Util/DateTimeExtension.cs
+++ SharpestChain/SharpestChain/Util/DateTimeExtension.cs
@@ -6,9 +6,7 @@
     {
         private static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1);
 
-        public static long ToUnixTimestamp(this DateTime timestamp)
-        {
-            return (long) DateTime.UtcNow.Subtract(UNIX_EPOCH).TotalSeconds;
-        }
+        public static long ToUnixTimestamp(this DateTime timestamp) => (long) timestamp.Subtract(UNIX_EPOCH).TotalSeconds;
+
     }
 }
