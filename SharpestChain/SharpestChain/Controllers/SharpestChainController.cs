f210d32305a46b6bb4f8191c76b79c09432850ec
Stefan Lauer <stefan.lauer@innoq.com>
2018-04-19 12:59:52 +0200

    Add default endpoints with dummy data

---
+++ SharpestChain/SharpestChain/Controllers/SharpestChainController.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpestChain.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("/")]
+    public class SharpestChainController : Controller
+    {
+        
+        
+        // GET /
+        [HttpGet]
+        //public IEnumerable<string> Get()
+        public object NodeInfo()
+        {
+            Response.ContentType = "application/json";
+            return new {nodeId = "bcfeb8c5-c9a6-4731-9a17-e0fedd7aa073", currentBlockHeight = 69};
+        }
+
+        // GET blocks
+        [HttpGet("blocks")]
+        public object Blocks()
+        {
+            Response.ContentType = "application/json";
+            return new { blocks = new []{"dummy"} };
+        }
+
+        // GET mine
+        [HttpGet("mine")]
+        public object Mine()
+        {
+            Response.ContentType = "application/json";
+            return new { mined = "true" };
+        }
+    }
+}
