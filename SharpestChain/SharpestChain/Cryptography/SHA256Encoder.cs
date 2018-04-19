77665f448cadaf6d0b68830a66da42b98b83959e
Ben Wolf <benjamin.wolf@innoq.com>
2018-04-19 15:14:51 +0200

    Fix SHA256 encoder

--- SharpestChain/SharpestChain/Cryptography/SHA256Encoder.cs
+++ SharpestChain/SharpestChain/Cryptography/SHA256Encoder.cs
@@ -1,28 +1,33 @@
 ﻿namespace Com.Innoq.SharpestChain.Cryptography
 {
+    using System.Linq;
     using System.Security.Cryptography;
     using System.Text;
 
     /// <summary>
     /// Class provides methods to encode any given string to a sha256 encoded string.
     /// </summary>
-    internal static class SHA256Encoder
+    public static class SHA256Encoder
     {
-
         private static readonly SHA256 encoder = SHA256.Create();
 
         /// <summary>
         /// Method encodes a given string to an SHA256 encoded string. If <paramref name="givenString"/> is <c>null</c> or empty,
-        /// an empty string is returned. For conversion, the ASCII bytes of the string are used. The resulting bytearry is again converted to
-        /// a string, based on ASCII encoding.
+        /// an empty string is returned. For conversion, the ASCII bytes of the string are used.
         /// </summary>
         /// <param name="givenString">String that will be converted.</param>
         /// <returns>SHA256 encoded version of <paramref name="givenString"/></returns>
-        internal static string EncodeString(string givenString)
+        public static string EncodeString(string givenString)
         {
-            return string.IsNullOrEmpty(givenString)
-                    ? string.Empty
-                    : Encoding.ASCII.GetString(encoder.ComputeHash(Encoding.ASCII.GetBytes(givenString)));
+            string hashString = string.Empty;
+
+            if (string.IsNullOrEmpty(givenString))
+            {
+                return hashString;
+            }
+
+            var hash = encoder.ComputeHash(Encoding.ASCII.GetBytes(givenString));
+            return hash.Aggregate(hashString, (current, x) => current + string.Format("{0:x2}", x));
         }
     }
 }
