40662fb84996658469e22c9569fcafbaf0549f35
David Caspar <david.caspar@aol.ch>
2018-04-19 12:31:51 +0200

    First version of data structures.

---
+++ SharpestChain/SharpestChain/data/Block.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpestChain.data
+{
+    using System;
+
+    public class Block
+    {
+        public long Index;
+        public long Timestamp;
+        public long Proof;
+        public Transpaction[] Transactions;
+        public string PreviousBlockHash;
+    }
+
+    public class Transpaction
+    {
+        public Guid Id;
+        public long Timestamp;
+        public string Payload;
+    }
+}
