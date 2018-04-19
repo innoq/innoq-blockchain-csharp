ba98bf57c19c263a032951474ef7e4ff00ab9f0a
Ben Wolf <benjamin.wolf@innoq.com>
2018-04-19 15:14:43 +0200

    Add unit test for SHA256 encoder

--- SharpestChain/SharpestChainTest/Cryptography/SHA256EncoderTest.cs
+++ SharpestChain/SharpestChainTest/Cryptography/SHA256EncoderTest.cs
@@ -1,7 +1,17 @@
 ﻿namespace Com.Innoq.SharpestChain.Cryptography
 {
+    using NUnit.Framework;
+
+    [TestFixture]
     public class SHA256EncoderTest
     {
 
+        [TestCase(null, "")]
+        [TestCase("", "")]
+        [TestCase("I am Peter Pan", "10a09abd7f9234503a9941139dcdbd1bc4b1705beb5c3702d4b08142beb0ac6a")]
+        public void TestEncoding(string originalString, string hash)
+        {
+            Assert.That(hash, Is.EqualTo(SHA256Encoder.EncodeString(originalString)));
+        }
     }
 }
