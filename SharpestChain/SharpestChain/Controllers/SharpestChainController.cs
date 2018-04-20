c06cb95fa8862ba3763bc4a7ea9df271c841f1fd
Ben Wolf <benjamin.wolf@innoq.com>
2018-04-20 11:35:05 +0200

    Greatly improve JSON handling

--- SharpestChain/SharpestChain/Controllers/SharpestChainController.cs
+++ SharpestChain/SharpestChain/Controllers/SharpestChainController.cs
@@ -1,67 +1,51 @@
 ﻿namespace Com.Innoq.SharpestChain.Controllers
 {
     using System;
-    using System.IO;
     using System.Linq;
     using System.Text;
+
+    using Blocks;
+
+    using Data;
 
     using IO;
 
     using Microsoft.AspNetCore.Mvc;
 
     using Newtonsoft.Json;
-        
-    using System.Threading.Tasks;
-
-    using Blocks;
-
-    using data;
 
     [Route("/")]
     public class SharpestChainController : Controller
     {
-      
         private readonly string _nodeId = Guid.NewGuid().ToString();
-        
+
         // GET /
         [HttpGet]
-        public async Task<IActionResult> NodeInfo()
+        public IActionResult NodeInfo()
         {
             var nodeInfo = new NodeInfo
                            {
                                    NodeId = _nodeId,
                                    CurrentBlockHeight = Persistence.Get().Last().Index
                            };
-            var byteArray = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(nodeInfo));
-            var stream = new MemoryStream(byteArray);
-            
-            return await Task.FromResult(new FileStreamResult(stream, "application/json"));
+
+            return Content(JsonConvert.SerializeObject(nodeInfo), "application/json", Encoding.UTF8);
         }
 
         // GET blocks
         [HttpGet("blocks")]
-        public async Task<IActionResult> Blocks()
+        public IActionResult Blocks()
         {
-            
-           var byteArray = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Persistence.Get()));
-           var stream = new MemoryStream(byteArray);
-            
-           return await Task.FromResult(new FileStreamResult(stream, "application/json"));
+            return Content(JsonConvert.SerializeObject(Persistence.Get()), "application/json", Encoding.UTF8);
         }
 
         // GET mine
         [HttpGet("mine")]
-        public async Task<IActionResult> Mine()
+        public IActionResult Mine()
         {
             var block = Miner.FindNewBlock(Persistence.Get().Last());
             Persistence.Append(block);
-           
-            var byteArray = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(block));
-            var stream = new MemoryStream(byteArray);
-            
-            return await Task.FromResult(new FileStreamResult(stream, "application/json"));
-            
-            
+            return Content(block.toJson(), "application/json", Encoding.UTF8);
         }
     }
 }
